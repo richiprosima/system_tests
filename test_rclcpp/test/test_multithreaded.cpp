4da94476062980a5078ee1059a376d0ece6eaffe
Jackie Kay <jackie@osrfoundation.org>
2015-12-04 17:12:31 -0800

    fix atomic instantiation

--- test_rclcpp/test/test_multithreaded.cpp
+++ test_rclcpp/test/test_multithreaded.cpp
@@ -248,7 +248,8 @@ static inline void multi_access_publisher(bool intra_process)
     {
       if (timer_counter.load() >= iterations) {
         timer.cancel();
-        std::atomic_uint i = 0;
+        std::atomic_uint i;
+        i = 0;
         // Wait for pending subscription callbacks to trigger.
         while (subscription_counter < timer_counter && ++i < executor.get_number_of_threads()) {
           rclcpp::utilities::sleep_for(1_ms);
